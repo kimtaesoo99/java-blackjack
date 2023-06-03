229fd14a9937b3af0a2b69b26b9b5d064cb16f96
kimtaesoo <kimtaesoo7@naver.com>
2023-06-03 19:24:14 +0900

    feat: 이름과 현재 보유중인 카드를 가지는 추상 클래스 참가자 추가

---
+++ src/main/java/domain/Participant.java
@@ -0,0 +1,33 @@
+package domain;
+
+
+import java.util.List;
+
+public abstract class Participant {
+
+    private final Name name;
+    private final Deck deck;
+
+    public Participant(final Name name) {
+        this.name = name;
+        this.deck = new Deck();
+    }
+
+    public List<Card> getCards() {
+        return deck.getCards();
+    }
+
+    public int getSumOfDeck() {
+        return deck.getSumOfValue();
+    }
+
+    public void add(Card card) {
+        deck.add(card);
+    }
+
+    public String getName() {
+        return name.getName();
+    }
+
+    public abstract boolean canDraw();
+}
