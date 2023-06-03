847f221dd7e80dc94de9f594984243267856e48b
kimtaesoo <kimtaesoo7@naver.com>
2023-06-03 19:22:00 +0900

    test: 이름을 포장한 Name 테스트 추가

---
+++ src/test/java/domain/NameTest.java
@@ -0,0 +1,31 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class NameTest {
+
+    private static final String NAME = "Alice";
+    private static final String BLANK = " ";
+
+    @Test
+    public void getName() {
+        //given
+        Name name = new Name(NAME);
+
+        //when
+        String AliceName = name.getName();
+
+        //then
+        Assertions.assertThat(AliceName).isEqualTo(NAME);
+    }
+
+    @Test
+    public void validationBlank() {
+        //given
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> new Name(BLANK))
+            .isInstanceOf(IllegalStateException.class);
+    }
+}
