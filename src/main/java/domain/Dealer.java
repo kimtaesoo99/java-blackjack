b96f7672752a2ba10dbd471f29740000107ed918
kimtaesoo <kimtaesoo7@naver.com>
2023-06-03 19:25:27 +0900

    feat: 참가자를 구현한 딜러 추가

---
+++ src/main/java/domain/Dealer.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Dealer extends Participant{
+
+    private static final int DRAW_STANDARD = 16;
+
+    public Dealer(final Name name) {
+        super(name);
+    }
+
+    @Override
+    public boolean canDraw() {
+        return getSumOfDeck() <= DRAW_STANDARD;
+    }
+}
