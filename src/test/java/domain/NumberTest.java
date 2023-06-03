512b777a9d9579d5dfede6a4435f6faee249459b
kimtaesoo <kimtaesoo7@naver.com>
2023-06-03 19:18:37 +0900

    test: enum 타입의 카드 숫자 테스트 추가

---
+++ src/test/java/domain/NumberTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NumberTest {
+
+    private static final String ACE_VALUE = "A";
+    private static final int MATCH_NUMBER = 13;
+
+    @Test
+    public void getValue() {
+        //given
+        Number number = Number.ACE;
+
+        //when
+        String value = number.getName();
+
+        //then
+        assertThat(value).isEqualTo(ACE_VALUE);
+    }
+
+    @Test
+    public void findMatchingNumber() {
+        //given
+
+        //when
+        Number matchNumber = Number.findMatchingNumber(MATCH_NUMBER);
+
+        //then
+        assertThat(matchNumber).isEqualTo(Number.ACE);
+    }
+}
