999e30c6a5503ec91c6981ef56e063eedb1f2422
kimtaesoo <kimtaesoo7@naver.com>
2023-06-03 19:27:04 +0900

    feat: 입력을 담당하는 InputView 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static final String INPUT_PLAYERS_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String readParticipantName() {
+        System.out.println(INPUT_PLAYERS_NAME);
+        return scanner.next();
+    }
+
+    public static String readAddMoreCard() {
+        try {
+            String command = scanner.next();
+            InputValidation.addMoreCardCommand(command);
+            return command;
+        } catch (IllegalStateException e) {
+            return readAddMoreCard();
+        }
+    }
+}
