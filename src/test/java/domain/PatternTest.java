6c789ea9201e083eb8687ccc5d3626f0fc56f1d2
kimtaesoo <kimtaesoo7@naver.com>
2023-06-03 19:17:53 +0900

    test: enum 타입의 카드 패턴 테스트 추가

---
+++ src/test/java/domain/PatternTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PatternTest {
+
+    private static final String DIAMOND = "다이아몬드";
+    private static final int MATCH_NUMBER = 3;
+
+    @Test
+    public void getName() {
+        //given
+        Pattern pattern = Pattern.DIAMOND;
+
+        //when
+        String name = pattern.getName();
+
+        //then
+        assertThat(name).isEqualTo(DIAMOND);
+    }
+
+    @Test
+    public void findMatchPattern() {
+        //given
+
+        //when
+        Pattern matchedPattern = Pattern.findMatchingPattern(MATCH_NUMBER);
+
+        //then
+        assertThat(matchedPattern).isEqualTo(Pattern.DIAMOND);
+    }
+}
