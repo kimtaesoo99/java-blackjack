a155674f71dbee1b6ed0ac36f956fffc9de2df65
kimtaesoo <kimtaesoo7@naver.com>
2023-06-05 20:18:50 +0900

    refactor: enum을 통해 검증로직을 수행하도록 수정

--- src/main/java/view/InputValidation.java
+++ src/main/java/view/InputValidation.java
@@ -1,14 +1,15 @@
 package view;
 
+import domain.Command;
+import exception.WrongCommandException;
+
 public class InputValidation {
 
-    private static final String YES = "y";
-    private static final String NO = "n";
     private static final String COMMAND_ERROR_MESSAGE = "y나 n을 입력해주세요.";
 
-    public static void addMoreCardCommand(String command) {
-        if (!(command.equals(YES) || command.equals(NO))) {
-            throw new IllegalStateException(COMMAND_ERROR_MESSAGE);
+    public static void checkCorrectCommand(String input) {
+        if (!Command.isCorrectInput(input)) {
+            throw new WrongCommandException(COMMAND_ERROR_MESSAGE);
         }
     }
 }
