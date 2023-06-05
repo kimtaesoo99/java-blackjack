f84777737c1b49fa828e032c368a81bb33c2de6c
kimtaesoo <kimtaesoo7@naver.com>
2023-06-05 20:12:50 +0900

    refactor: 정적팩토리 메서드를 적용하여 이름을 명확하게 나타냄

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -5,9 +5,18 @@ public class Card {
     private final Pattern pattern;
     private final Number number;
 
-    public Card(final int matchingNumber) {
+    public static Card createWithMatchNumber(final int matchingNumber) {
+        return new Card(matchingNumber);
+    }
+
+    private Card(final int matchingNumber) {
         this.pattern = Pattern.findMatchingPattern(matchingNumber);
         this.number = Number.findMatchingNumber(matchingNumber);
+    }
+
+    public Card(final Pattern pattern, final Number number) {
+        this.pattern = pattern;
+        this.number = number;
     }
 
     public String getPattern() {
