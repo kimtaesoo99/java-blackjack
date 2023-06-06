15c0c13cd983b573818a89a9183b6e179fca64b0
kimtaesoo <kimtaesoo7@naver.com>
2023-06-06 22:19:33 +0900

    feat: 플레이어들을 일급컬렉션으로 관리하는 Players 추가

---
+++ src/main/java/domain/Players.java
@@ -0,0 +1,42 @@
+package domain;
+
+import exception.DuplicateNameException;
+
+import java.util.*;
+
+public class Players {
+
+    private static final String DUPLICATE_ERROR_MESSAGE = "플레이어 이름은 중복이 될 수 없습니다.";
+
+    private final List<Player> players;
+
+    public Players(final List<Player> players) {
+        validateDuplicateName(players);
+        this.players = players;
+    }
+
+    private void validateDuplicateName(final List<Player> players) {
+        Set<String> names = new HashSet<>();
+        for (final Player player : players) {
+            names.add(player.getName());
+        }
+
+        if (names.size() != players.size()) {
+            throw new DuplicateNameException(DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    public List<Player> getPlayers() {
+        return Collections.unmodifiableList(players);
+    }
+
+    public List<String> getPlayersName() {
+        List<String> names = new ArrayList<>();
+        players.forEach(player -> names.add(player.getName()));
+        return Collections.unmodifiableList(names);
+    }
+
+    public void initCardSetting(final Cards cards) {
+        players.forEach(player -> player.initCardSetting(cards));
+    }
+}
