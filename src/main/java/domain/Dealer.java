11aa54d91eec4c48d41b60a24b2903c26b801850
kimtaesoo <kimtaesoo7@naver.com>
2023-06-10 22:26:39 +0900

    refactor: Amount를 인스턴스 변수로 설정

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -4,8 +4,19 @@ public class Dealer extends Participant {
 
     private static final int DRAW_STANDARD = 16;
 
-    public Dealer(final Name name) {
+    private Amount amount;
+
+    public Dealer(final Name name, final Amount amount) {
         super(name);
+        this.amount = amount;
+    }
+
+    public void addAmount(int amount) {
+        this.amount.add(amount);
+    }
+
+    public int getAmount() {
+        return amount.getAmount();
     }
 
     @Override
--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,11 +1,27 @@
 package domain;
 
-public class Player extends Participant{
+public class Player extends Participant {
 
     private static final int DRAW_STANDARD = 21;
+    private static final double MULTIPLY_NUMBER = 1.5;
 
-    public Player(final Name name) {
+    private final Amount amount;
+
+    public Player(final Name name, final Amount amount) {
         super(name);
+        this.amount = amount;
+    }
+
+    public void multiplyAmount() {
+        amount.multiply(MULTIPLY_NUMBER);
+    }
+
+    public void setAmount(final int betAmount) {
+        this.amount.setAmount(betAmount);
+    }
+
+    public int getAmount() {
+        return amount.getAmount();
     }
 
     @Override
