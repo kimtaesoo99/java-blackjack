6fa1cafa64cf72f0ff6c1f601416cb9b6407ed61
kimtaesoo <kimtaesoo7@naver.com>
2023-06-10 22:28:57 +0900

    test: 정적 팩토리 메서드를 사용한 테스트로 변경

--- src/test/java/domain/DealerTest.java
+++ src/test/java/domain/DealerTest.java
@@ -9,14 +9,18 @@ class DealerTest {
 
     private static final String DEALER = "딜러";
     private static final int DECK_SUM = 20;
+    private static final int INIT_AMOUNT = 0;
+    private static final int CLOVER_JACK = 38;
+    private static final int SPADE_JACK = 25;
 
     private Dealer dealer;
 
     @BeforeEach
     void beforeEach() {
         Name name = new Name(DEALER);
-        dealer = new Dealer(name);
-        Card card = new Card(Pattern.CLOVER, Number.KING);
+        Amount amount = new Amount(INIT_AMOUNT);
+        dealer = new Dealer(name, amount);
+        Card card = Card.createWithMatchNumber(CLOVER_JACK);
         dealer.add(card);
     }
 
@@ -32,7 +36,7 @@ class DealerTest {
     @Test
     public void canDrawFail() {
         //given
-        Card card2 = new Card(Pattern.HEART, Number.JACK);
+        Card card2 = Card.createWithMatchNumber(SPADE_JACK);
         dealer.add(card2);
 
         //when
@@ -45,7 +49,7 @@ class DealerTest {
     @Test
     public void getSumOfDeck() {
         //given
-        Card card2 = new Card(Pattern.HEART, Number.JACK);
+        Card card2 = Card.createWithMatchNumber(SPADE_JACK);
         dealer.add(card2);
 
         //when
