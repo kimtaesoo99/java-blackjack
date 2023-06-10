fdadf3fa6fefa798beacc07b4f6c92d102479fc1
kimtaesoo <kimtaesoo7@naver.com>
2023-06-10 22:30:26 +0900

    refactor: 필요없는 로직 삭제 및 금액 출력부분 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,33 +1,35 @@
 package view;
 
-import dto.ParticipantResponseDto;
-import dto.PlayersNameResponseDto;
+import dto.DealerAmountResponse;
+import dto.ParticipantResponse;
+import dto.PlayerAmountResponse;
+import dto.PlayersNameResponse;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringJoiner;
 
 public class OutputView {
 
-    private static final String FINAL_RESULT = "## 최종 승패";
+    private static final String FINAL_RESULT = "## 최종 수익";
     private static final String DRAW_TWO_CARD = "에게 2장을 나누었습니다.";
     private static final String DRAW_ONE_CARD = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
     private static final String DEALER_ONE_MORE_CARD = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private static final String BET_AMOUNT = "의 배팅 금액은?";
     private static final String CARD = "카드: ";
     private static final String SEPARATOR = ", ";
     private static final String RESULT = " - 결과: ";
     private static final String COLON = ": ";
     private static final String NEXT_LINE = "\n";
     private static final String AND = "와 ";
-    private static final String EMPTY = "";
-    private static final String WIN = "승";
-    private static final String DRAW = "무";
-    private static final String LOSE = "패";
     private static final String BLANK = " ";
     private static final String DEALER = "딜러";
     private static final int FIRST_CARD = 0;
 
-    public static void printInitCardSetting(final PlayersNameResponseDto participants) {
+    public static void printBetAmount(final String playerName) {
+        System.out.println(NEXT_LINE + playerName + BET_AMOUNT);
+    }
+
+    public static void printInitCardSetting(final PlayersNameResponse participants) {
         StringJoiner players = new StringJoiner(SEPARATOR);
 
         participants.getPlayersName()
@@ -36,19 +38,19 @@ public class OutputView {
         System.out.println(NEXT_LINE + DEALER + AND + players + DRAW_TWO_CARD);
     }
 
-    public static void printDealerFirstCard(final ParticipantResponseDto dealer) {
+    public static void printDealerFirstCard(final ParticipantResponse dealer) {
         System.out.println(dealer.getName() + COLON + dealer.getCardsName().get(FIRST_CARD));
     }
 
-    public static void printPlayerCards(final ParticipantResponseDto player) {
+    public static void printPlayerCards(final ParticipantResponse player) {
         System.out.println(player.getName() + CARD + sortCards(player.getCardsName()));
     }
 
-    public static void printDealerSumOfCard(final ParticipantResponseDto dealer) {
+    public static void printDealerSumOfCard(final ParticipantResponse dealer) {
         System.out.println(dealer.getName() + BLANK + CARD + sortCards(dealer.getCardsName()) + RESULT + dealer.getSumOfCards());
     }
 
-    public static void printPlayerSumOfCard(final ParticipantResponseDto player) {
+    public static void printPlayerSumOfCard(final ParticipantResponse player) {
         System.out.println(player.getName() + CARD + sortCards(player.getCardsName()) + RESULT + player.getSumOfCards());
     }
 
@@ -68,7 +70,7 @@ public class OutputView {
         System.out.println(error);
     }
 
-    public static void printChooseAddMoreCard(final ParticipantResponseDto player) {
+    public static void printChooseAddMoreCard(final ParticipantResponse player) {
         System.out.println(player.getName() + DRAW_ONE_CARD);
     }
 
@@ -80,40 +82,11 @@ public class OutputView {
         System.out.println(NEXT_LINE + FINAL_RESULT);
     }
 
-    public static void printPlayerResult(final String result, final ParticipantResponseDto player) {
-        System.out.println(player.getName() + COLON + result);
+    public static void printDealerResult(final DealerAmountResponse dealer) {
+        System.out.println(dealer.getName() + COLON + dealer.getBetAmount());
     }
 
-    public static void printDealerResult(final String results) {
-        String sortedResult = getSortResult(results);
-        System.out.println(DEALER + COLON + sortedResult);
-    }
-
-    private static String getSortResult(final String results) {
-        int winCount = getContainCount(WIN, results);
-        int drawCount = getContainCount(DRAW, results);
-        int loseCount = getContainCount(LOSE, results);
-
-        return sortResult(winCount, drawCount, loseCount);
-    }
-
-    private static int getContainCount(String resultStatus, String results) {
-        return (int) Arrays.stream(results.split(EMPTY))
-            .filter(result -> result.equals(resultStatus))
-            .count();
-    }
-
-    private static String sortResult(int winCount, int drawCount, int loseCount) {
-        StringBuilder results = new StringBuilder();
-        addResult(winCount, results, WIN);
-        addResult(drawCount, results, DRAW);
-        addResult(loseCount, results, LOSE);
-        return results.toString();
-    }
-
-    private static void addResult(int count, StringBuilder results, String result) {
-        if (count != 0) {
-            results.append(count).append(result).append(BLANK);
-        }
+    public static void printPlayerResult(final PlayerAmountResponse player) {
+        System.out.println(player.getName() + COLON + player.getBetAmount());
     }
 }
