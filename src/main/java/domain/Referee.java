f1dc74fa429809cfaf7059e3f52dfc795bbc5817
kimtaesoo <kimtaesoo7@naver.com>
2023-06-10 22:28:32 +0900

    feat: 수익을 분배해주는 로직 추가

--- src/main/java/domain/Referee.java
+++ src/main/java/domain/Referee.java
@@ -6,32 +6,65 @@ public class Referee {
     private static final String DRAW = "무";
     private static final String LOSE = "패";
     private static final int BLACK_JACK = 21;
+    private static final int MINUS = -1;
+    private static final int ZERO_AMOUNT = 0;
 
     public String compareSumOfCard(final int firstSum, final int secondSum) {
         return compare(firstSum, secondSum);
     }
 
+    public boolean checkBlackJack(final int playerCardOfSum) {
+        return playerCardOfSum == BLACK_JACK;
+    }
+
     private String compare(final int firstSum, final int secondSum) {
-        if (winCase(firstSum, secondSum)) {
+        if (isWinCase(firstSum, secondSum)) {
             return WIN;
         }
-        if (drawCase(firstSum, secondSum)) {
+        if (isDrawCase(firstSum, secondSum)) {
             return DRAW;
         }
         return LOSE;
     }
 
-    private boolean winCase(final int firstSum, final int secondSum) {
+    private boolean isWinCase(final int firstSum, final int secondSum) {
         if (firstSum > secondSum && firstSum <= BLACK_JACK) {
             return true;
         }
         return secondSum > BLACK_JACK && firstSum <= BLACK_JACK;
     }
 
-    private boolean drawCase(final int firstSum, final int secondSum) {
+    private boolean isDrawCase(final int firstSum, final int secondSum) {
         if (firstSum == secondSum) {
             return true;
         }
         return firstSum > BLACK_JACK && secondSum > BLACK_JACK;
+    }
+
+    public void distributeRevenue(final String result, final Player player, final Dealer dealer) {
+        int amount = player.getAmount();
+
+        distributeWinCase(result, dealer, amount);
+        distributeDrawCase(result, player);
+        distributeLoseCase(result, player, dealer, amount);
+    }
+
+    private void distributeWinCase(final String result, final Dealer dealer, final int amount) {
+        if (result.equals(WIN)) {
+            dealer.addAmount(amount * MINUS);
+        }
+    }
+
+    private void distributeDrawCase(final String result, final Player player) {
+        if (result.equals(DRAW)) {
+            player.setAmount(ZERO_AMOUNT);
+        }
+    }
+
+    private void distributeLoseCase(final String result, final Player player, final Dealer dealer, final int amount) {
+        if (result.equals(LOSE)) {
+            player.setAmount(amount * MINUS);
+            dealer.addAmount(amount);
+        }
     }
 }
